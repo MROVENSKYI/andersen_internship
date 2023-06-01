a94a292526e4e4d9f687c3e454257a65d6ddcf30
MROVENSKYI <markrovensky@gmail.com>
2023-06-01 15:30:00 +0300

    auth reg

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+
+
+
+
+class AuthController extends Controller
+{
+
+
+   public function register(RegisterRequest $request)  { 
+    $validator= Validator::make($request->all(),[
+        'name' => 'required|min:3',
+        'email' => 'required|email',
+        'password' => 'required|min:6|max:100',
+        'confirm_password' => 'required|same:password'
+    ]);
+    if ($validator->fails()) {
+        return response() ->json([
+            'errors' =>$validator->errors()
+        ],422);
+    }
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,]);
+    $token = $user->createToken('API Token')->accessToken;
+
+     return response()->json([
+        'token' => $token
+    ],201);
+   
+        
+    }
+
+}
+   
