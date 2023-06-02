130005c850ec7d55272959f857db8b88db7d2f83
MROVENSKYI <markrovensky@gmail.com>
2023-06-02 14:59:17 +0300

    auth:reg newfix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,12 +14,7 @@ class AuthController extends Controller
     
    public function register(RegisterRequest $request)  { 
 
-    $user = User::create($request->validated([
-        'email' => $request->email,
-        'password' => $request->password,
-        'confirm_password' => $request->password,
-        ]
-    ));
+    $user = User::create($request->validated());
 
     $token = $user->createToken('API Token')->accessToken;
 
--- config/database.php
+++ config/database.php
@@ -46,12 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'app_andersen'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -61,6 +60,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'unix_socket' => '/tmp/mysql.sock',
         ],
 
         'pgsql' => [
