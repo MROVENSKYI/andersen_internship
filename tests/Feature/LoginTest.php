4da8c12b0d1044dc3a17b5b3998f0f260579b830
MROVENSKYI <markrovensky@gmail.com>
2023-06-07 15:02:21 +0300

    login-test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -12,5 +12,19 @@ use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-   
+    use DatabaseMigrations;
+    use RefreshDatabase;
+    /** @test */
+    public function it_should_login_user()
+    {   
+        $clientRepository = new ClientRepository();
+        $clientRepository->createPersonalAccessClient(
+            null,
+            'Personal Access Client',
+            'http://example.com/callback'
+        );
+        $user = User::factory()->make();
+        $response = $this->actingAs($user)->json('POST', route('auth.login'), $user->getAttributes())->assertStatus(200);
+        $response->assertJsonStructure(['token']);
+    }
 } 
